1757e61e808f8523baa5d1f407bdee98f923c55b
v. kudryashov <kudryashovva@rtinfosys.ru>
2024-02-11 17:31:52 +0400

    Start formatDuration

---
+++ src/main/java/com/github/class13/durationformat/TimeFormatter.java
@@ -0,0 +1,9 @@
+package com.github.class13.durationformat;
+
+public class TimeFormatter {
+    //read more: https://www.codewars.com/kata/52742f58faf5485cae000b9a
+    public static String formatDuration(int seconds) {
+
+        return "";
+    }
+}
