43675231c8e1e0b67168a19f2bd1cb3d20b01984
v. kudryashov <kudryashovva@rtinfosys.ru>
2024-02-11 17:07:03 +0400

    First solution of SquareUp

--- src/main/java/com/github/class13/arraysquareup/SquareUp.java
+++ src/main/java/com/github/class13/arraysquareup/SquareUp.java
@@ -5,10 +5,34 @@ import java.util.Arrays;
 public class SquareUp {
 
     public static int[] squareUp(int n) {
-        return new int[n * n];
+        if (n == 1) {
+            return new int[]{1};
+        }
+        int[] array = new int[n * n];
+        int zc = 1, dc = 0, zBlockSize = n - 1, dBlockSize = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (dc != 0) {
+                array[i] = dc;
+                dc--;
+                if (dc == 0 && zBlockSize == 0) {
+                    dBlockSize = n;
+                    dc = dBlockSize;
+                }
+                continue;
+            }
+            if (zc == zBlockSize) {
+                zBlockSize--;
+                dBlockSize++;
+                dc = dBlockSize;
+                zc = 1;
+            } else {
+                zc++;
+            }
+        }
+        return array;
     }
 
     public static void main(String... args) {
-        System.out.println(Arrays.toString(squareUp(4)));
+        System.out.println(Arrays.toString(squareUp(1)));
     }
 }
